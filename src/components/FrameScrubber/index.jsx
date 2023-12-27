18769c3641b322a902a3283ccc854adee6b2fd66
Eric Chung <rchung.eric@gmail.com>
2023-12-27 19:24:42 +1100

    Remove react-rangeslider

--- src/components/FrameScrubber/index.jsx
+++ src/components/FrameScrubber/index.jsx
@@ -1,27 +1,28 @@
 import { useContext } from 'react';
-import Slider from 'react-rangeslider';
 import { CapturerContext, NUM_FRAMES } from '../../contexts/CapturerContext';
 import { clamp } from '../../helpers/math';
-import 'react-rangeslider/lib/index.css';
 import './styles.scss';
 
 const FrameScrubber = () => {
   const { useScrubber, scrubberFrame, setScrubberFrame } = useContext(CapturerContext);
-  const handleChange = (value) => {
-    setScrubberFrame(clamp(value, 0, NUM_FRAMES));
+  const handleChange = (e) => {
+    setScrubberFrame(clamp(e.target.value, 0, NUM_FRAMES));
   };
   if (!useScrubber) return null;
   return (
     <div className="frame-scrubber">
-      <Slider min={0} max={NUM_FRAMES} step={1} value={scrubberFrame} onChange={handleChange} className='frame-scrubber__input' />
+      {/* Range slider */}
+      <input
+        type="range"
+        min={0}
+        max={NUM_FRAMES}
+        step={1}
+        value={scrubberFrame}
+        onChange={handleChange}
+        className="frame-scrubber__input"
+      />
       <div>
-        Frame:{' '}
-        <input
-          type="number"
-          value={scrubberFrame}
-          onChange={(e) => handleChange(e.target.value)}
-          className="frame-input"
-        />
+        Frame: <input type="number" value={scrubberFrame} onChange={handleChange} className="frame-input" />
       </div>
     </div>
   );
